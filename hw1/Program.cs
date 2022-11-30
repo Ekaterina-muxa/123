c0414279710fe2140fa803af67ab9af520331a64
«Ekaterina» <katuxa-muxa@mail.ru>
2022-11-30 21:47:25 +0200

    первая задача

---
+++ hw1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int CountSumDigit (int num)
+{
+    int sum = 0;
+    while (num >= 1)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+        
+    }
+
+     return sum;
+
+}
+Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(CountSumDigit(number));
