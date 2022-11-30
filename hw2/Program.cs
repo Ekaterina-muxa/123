b4b32aa5db3bcd9ec0b24a33d38994da229c3d72
«Ekaterina» <katuxa-muxa@mail.ru>
2022-11-30 21:50:18 +0200

    вторая задача

---
+++ hw2/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+
+int FindDegree (int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result*a;
+    }
+    return result;
+}
+Console.WriteLine("введите число a");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число b");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int fin = FindDegree(number1, number2);
+Console.WriteLine(fin);
